f3a25056b880c46b273b0a6ae67902c6a18c6599
piotrk39 <piotrk39@gmail.com>
2023-01-22 20:59:41 +0200

    Update CV remove unused files

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/../Assets/CV Piotr Krajewski.pdf";
+import pdf from "../../Assets/../Assets/CV_Piotr_Int.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
